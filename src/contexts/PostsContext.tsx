5cd31c1ea0dbae686093359c008771c926de3f80
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-05-02 17:39:59 -0300

    fix: adjust callback dependences

--- src/contexts/PostsContext.tsx
+++ src/contexts/PostsContext.tsx
@@ -21,33 +21,39 @@ export function PostsProvider({ children }: IPostsProviderProps) {
     }
   }, []);
 
-  const createPost = useCallback(async (user: string, data: PostFormData) => {
-    try {
-      const response = await handleCreatePost({
-        username: user,
-        title: data.title,
-        content: data.content,
-      });
+  const createPost = useCallback(
+    async (user: string, data: PostFormData) => {
+      try {
+        const response = await handleCreatePost({
+          username: user,
+          title: data.title,
+          content: data.content,
+        });
 
-      setPosts((prevPosts) => [...prevPosts, response.data]);
-      await fetchPosts();
-    } catch (error) {
-      console.error("Error creating post:", error);
-    }
-  }, []);
+        setPosts((prevPosts) => [...prevPosts, response.data]);
+        await fetchPosts();
+      } catch (error) {
+        console.error("Error creating post:", error);
+      }
+    },
+    [fetchPosts]
+  );
 
-  const updatePost = useCallback(async (id: number, data: PostFormData) => {
-    try {
-      await handleUpdatePost(id, {
-        title: data.title,
-        content: data.content,
-      });
+  const updatePost = useCallback(
+    async (id: number, data: PostFormData) => {
+      try {
+        await handleUpdatePost(id, {
+          title: data.title,
+          content: data.content,
+        });
 
-      await fetchPosts();
-    } catch (error) {
-      console.error("Error updating post:", error);
-    }
-  }, []);
+        await fetchPosts();
+      } catch (error) {
+        console.error("Error updating post:", error);
+      }
+    },
+    [fetchPosts]
+  );
 
   const deletePost = useCallback(async (id: number) => {
     try {

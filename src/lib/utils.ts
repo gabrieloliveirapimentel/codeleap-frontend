d9e4b319c6e36169e7239b9902c7ce30cb0c5546
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-05-02 17:56:53 -0300

    feat: insert suffix in formatted date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,9 @@
 import { formatDistanceToNow } from "date-fns";
 
 export function dateToNow(date: string) {  
-  const formattedDate = formatDistanceToNow(new Date(date));
+  const formattedDate = formatDistanceToNow(new Date(date), {
+    addSuffix: true
+  });
 
   return formattedDate;
 }
